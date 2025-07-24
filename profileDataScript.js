a97384105d000227f4967528bec64cc27f29468e
venkatnadella <indolar23@gmail.com>
2025-07-24 16:32:33 -0400

    Updated styling and added download resume button

--- profileDataScript.js
+++ profileDataScript.js
@@ -16,26 +16,20 @@ fetch('profileData.json')
         </div>
       </div>
       <h3>
-        <a href="mailto:${data.email}">${data.email}</a><br>
-        <a href="tel:${data.phone}">${data.phone}</a><br>
-        ${data.location && typeof data.location === 'object' ? `${data.location.city}, ${data.location.province}` : data.location}<br>
-        <a href="${data.github}">${data.github}</a><br>
-        <a href="${data.linkedin}">${data.linkedin}</a>
+        Email: <a href="mailto:${data.email}">${data.email}</a><br>
+        Mobile: <a href="tel:${data.phone}">${data.phone}</a><br>
+        Location: ${data.location && typeof data.location === 'object' ? `${data.location.city}, ${data.location.province}` : data.location}<br>
+        GitHub: <a href="${data.github}">Venkat Nadella</a><br>
+        Linkedin: <a href="${data.linkedin}">Venkata Sai Nadella</a>
       </h3>
       <p>${data.summary}</p>
-      <h4>TECHNICAL SKILLS</h4>
-      <ul>
-        ${data.skills.map(skillCat => `
-          <li><b>${skillCat.title} -</b> ${skillCat.skills.join(', ')}.</li>
-        `).join('')}
-      </ul>
     `;
 
         // Experience Section
         if (data.experience && data.experience.length > 0) {
             html += `<h4 id="experience">PROFESSIONAL EXPERIENCE</h4>`;
             data.experience.forEach(exp => {
-                html += `<p><b>${exp.company}</b><br><b>${exp.role}</b></p>`;
+                html += `<p><b>${exp.company}</b> - ${exp.location}<br><b>${exp.role}</b> | ${exp.domain} | ${exp.period}</p>`;
                 if (exp.projects && exp.projects.length > 0) {
                     exp.projects.forEach(proj => {
                         html += `<p><b>${proj.name}</b>`;
@@ -53,7 +47,7 @@ fetch('profileData.json')
         if (data.education && data.education.length > 0) {
             html += `<h4 id="education">EDUCATION</h4><ul>`;
             data.education.forEach(edu => {
-                html += `<li><b>${edu.degree}</b><br><b>${edu.institution}</b> | ${edu.period}</li>`;
+                html += `<li><b>${edu.degree}</b> - ${edu.specialization}&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<b>${edu.grade}</b> <sub>${edu.remarks}</sub><br><b>${edu.institution}</b> | ${edu.period}</li>`;
             });
             html += `</ul>`;
         }
@@ -62,13 +56,23 @@ fetch('profileData.json')
         if (data.projects && data.projects.length > 0) {
             html += `<h4 id="projects">PROJECTS</h4><ul>`;
             data.projects.forEach(proj => {
-                html += `<li><b>${proj.name}</b> - ${proj.description}`;
-                if (proj.techStack) html += `<br><b>Tech Stack:</b> ${proj.techStack}`;
-                if (proj.link) html += `<br><a href="${proj.link}">${proj.link}</a>`;
-                html += `</li>`;
+                html += `<li><b>${proj.name}</b> - ${proj.summary}`;
+                if (proj.description) html += `<ul>`;
+                proj.description.forEach(line => {
+                    html += `<li>${line}</li>`;})
+                html += `</ul>`;
+                if (proj.techStack) html += `<b>Tech Stack:</b> ${proj.techStack}`;
+                html += `</li><br>`;
             });
             html += `</ul>`;
         }
+
+        html += `<h4 id="skills">TECHNICAL SKILLS</h4>
+      <ul>
+        ${data.skills.map(skillCat => `
+          <li><b>${skillCat.title} -</b> ${skillCat.skills.join(', ')}.</li>
+        `).join('')}
+      </ul>`;
 
         parentDiv.innerHTML = html;
     });
